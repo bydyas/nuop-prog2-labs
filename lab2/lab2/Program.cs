a913909e6df070e5e9d71b1986d66fbddb2e9c90
bydyas <bogdan.shkaran@gmail.com>
2021-09-15 19:35:38 +0300

    initial

---
+++ lab2/lab2/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace lab2
+{
+    public class BitString
+    {
+        private int _length;
+        private int[] _vars;
+
+        public BitString() {}
+        public BitString(int bit_length)
+        {
+            Length = bit_length;
+            Vars = Generate_Random_Bit_Vars(_length);
+        }
+        public BitString(int bit_length, int[] bit_values)
+        {
+            Length = bit_length;
+            Vars = bit_values;
+        }
+
+
+
+        public int Length
+        {
+            get { return this._length; }
+            set { this._length = value; }
+        }
+
+        public int[] Vars
+        {
+            get { return this._vars; }
+            set { this._vars = value; }
+        }
+
+
+        // generating a random array of bit variables (0,1) when constructor has only one argument;
+        public int[] Generate_Random_Bit_Vars(int len)
+        {
+            int[] bit_store = new int[len];
+            Random rnd = new Random();
+
+            for (int i = 0; i < len; i++)
+            {
+                int bit_value = rnd.Next(0, 1);
+                bit_store[i] = bit_value;
+            }
+
+            return bit_store;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // testing constructors
+            BitString b_str = new BitString(4);
+            Console.WriteLine("[{0}]", string.Join(", ", b_str.Vars));
+        }
+    }
+}
